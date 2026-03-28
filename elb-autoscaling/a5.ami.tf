a8a9738ed24819a509f9fa5124eea5dc4adb6962
gopal <gopal1409@gmail.com>
2026-03-28 16:38:10 +0530

    terraform project

---
+++ elb-autoscaling/a5.ami.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "amazon_linux" {
+
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+#this will automatically find the ami
---
+++ provider.tf
@@ -0,0 +1,15 @@
+##using terraform we will create instance in aaws
+#provider will be aws
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
